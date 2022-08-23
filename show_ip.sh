e0ff99db11738019553e3379da6367543b6ed43c
Sudo-Chazz <73296244+Sudo-Chazz@users.noreply.github.com>
2022-08-23 15:33:13 -0400

    update
    added a prompt that will now show when not connected to a vpn

--- show_ip.sh
+++ show_ip.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-ip a show tun0 | egrep -o '([0-9]{1,3}[\.]){3}[0-9]{1,3}' | sed -n '1p'
+
+if nmcli con show --active | grep -q vpn; then
+	
+	ip a show tun0 | egrep -o '([0-9]{1,3}[\.]){3}[0-9]{1,3}' | sed -n '1p' 
+else
+	echo "VPN inactive"
+fi
